d4c8c69d893b7fe6cbd14ece15b446050fc4d8dc
Eeooor <kahanovlior@yahoo.com>
2023-11-28 16:48:53 +0000

    Source Code Cleaned up

--- Assets/Scripts/Atom_Movement.cs
+++ Assets/Scripts/Atom_Movement.cs
@@ -2,10 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectManager : MonoBehaviour
+public class Atom_Movement : MonoBehaviour
 {
-    // This entire thing is based on the Unity Doucumentation code https://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html
-    // Did start out with https://forum.unity.com/threads/how-to-find-the-nearest-object.360952/ which... didn't work but got me on the right track
+    Vector3 AtomMovement;
+    Vector3 Target = Vector3.zero;
+
+    Rigidbody2D Atom_Rigidbody;
+
+    GameObject TargetObject;
+
+    float AtomMovementSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AtomMovement = new Vector3();
+
+        Atom_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    //Fixed Update is used for physics as framerate and physics are updated at different intervals 
+    private void FixedUpdate()
+    {
+        //TargetObject = getNearestObject();
+
+        TargetObject = getNearestObject("Object");
+
+        Target = TargetObject.transform.position;
+
+        Vector3 direction = (Target - transform.position).normalized;  //Gets direction of target
+
+        Atom_Rigidbody.velocity = direction * AtomMovementSpeed;    //Sets Object velocity
+    }
+
     public GameObject getNearestObject(string withTag)
     {
         GameObject[] AllObjects; // for use in for loop
