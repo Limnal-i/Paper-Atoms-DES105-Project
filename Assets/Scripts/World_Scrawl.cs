7df4f7f970a35e33d647fae065cf26141e5bf7d5
Eeooor <kahanovlior@yahoo.com>
2023-11-30 14:17:31 +0000

    Bounds established

---
+++ Assets/Scripts/World_Scrawl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World_Scrawl : MonoBehaviour
+{
+
+    [SerializeField] Transform playerPosition;
+
+    Vector2Int positionOnWorld;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
