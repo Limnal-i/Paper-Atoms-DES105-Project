040f02254b588cd687a79467ddab44b344358741
Eeooor <kahanovlior@yahoo.com>
2023-12-05 14:04:35 +0000

    removed clutter

--- Assets/Scripts/Spawn_Manager.cs
+++ Assets/Scripts/Spawn_Manager.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using static UnityEngine.UI.Image;
-using UnityEngine.UIElements;
+
 
 public class Spawn_Manager : MonoBehaviour
 {
